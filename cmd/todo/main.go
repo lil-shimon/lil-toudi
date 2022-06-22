5e541c4899f4951bdc3ddfb78f0424bcca47ceba
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-06-22 23:26:55 +0900

    feat: 🎸 added two flags

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/lil-shimon/toudi"
+)
+
+const (
+	toudisFile = "toudis.json"
+)
 
 func main() {
-    fmt.Println("Hello world")
+
+	add := flag.Bool("add", false, "add a new toudis")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+
+	flag.Parse()
+
+	toudis := &toudi.Toudis{}
+
+	if err := toudis.Load(toudisFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		toudis.Add("Test")
+		err := toudis.Store(toudisFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := toudis.Complete(*complete)
+		err = toudis.Store(toudisFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
+
 }
