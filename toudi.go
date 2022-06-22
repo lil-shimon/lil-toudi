acff3a5754e77b3c7d9ca35da3bf6e3882490d45
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-06-22 23:14:00 +0900

    feat: 🎸 added load, write

--- toudi.go
+++ toudi.go
@@ -1,50 +1,85 @@
 package toudi
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
 type toudi struct {
-    Task string
-    Done bool
-    CreatedAt time.Time
-    CompletedAt time.Time
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
 }
 
 type Toudis []toudi
 
 func (t *Toudis) Add(task string) {
-    todo := toudi {
-        Task: task,
-        Done: false,
-        CreatedAt: time.Now(),
-        CompletedAt: time.Time{},
-    }
+	todo := toudi{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
 
-    *t = append(*t, todo)
+	*t = append(*t, todo)
 }
 
 func (t *Toudis) Complete(index int) error {
-    ls := *t
-    if index <= 0 || index > len(ls) {
-        return errors.New("invalid index")
-    }
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
 
-    ls[index - 1].CompletedAt = time.Now()
-    ls[index - 1].Done = true
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
 
-    return nil
+	return nil
 }
 
-func (t *Toudis) Delete (index int) error {
-    ls := *t
+func (t *Toudis) Delete(index int) error {
+	ls := *t
 
-    if index <= 0 || index >len(ls) {
-        return errors.New("invalid index")
-    }
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
 
-    *t = append(ls[:index-1], ls[index:]...)
+	*t = append(ls[:index-1], ls[index:]...)
 
-    return nil
+	return nil
+}
+
+func (t *Toudis) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return err
+	}
+
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Toudis) Store(filename string) error {
+	data, err := json.Marshal(t)
+
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, data, 0644)
 }
